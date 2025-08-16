0f8e4676c472f9a63c5ff9f5ce87350ff1fb60b8
Shinya Sakae <ss49919201@gmail.com>
2025-08-16 17:43:51 +0900

    feat: dummy monto searcher

---
+++ app/web/worker/query/monto.ts
@@ -0,0 +1,20 @@
+export const searchMontos = async () => {
+  return [
+    {
+      name: "A",
+      addresses: [
+        {
+          url: "http://example.com",
+        },
+      ],
+    },
+    {
+      name: "B",
+      addresses: [
+        {
+          url: "http://example.com",
+        },
+      ],
+    },
+  ];
+};
