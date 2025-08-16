87c1e8ba00a617f854ed876c9b429251f4276cbe
Shinya Sakae <ss49919201@gmail.com>
2025-08-16 12:18:18 +0900

    feat: use Effect.ts

--- app/web/worker/domain/event/monto.ts
+++ app/web/worker/domain/event/monto.ts
@@ -1,35 +1,84 @@
-import { Effect } from "effect";
+import { Effect, Match } from "effect";
 import { type DeepReadonly } from "ts-essentials";
 
-export type MontoEvent = MontoCreatedEvent;
+export type MontoEvent = MontoRegisteredEvent | MontoRemovedEvent;
 
-export type MontoCreatedEvent = DeepReadonly<{
-  type: "MontoCreatedEvent";
+export type MontoRegisteredEvent = DeepReadonly<{
+  readonly _tag: "MontoRegisteredEvent";
   name: string;
   address_urls: string[];
+}>;
+
+export type MontoRemovedEvent = DeepReadonly<{
+  readonly _tag: "MontoRemovedEvent";
+  reason: string;
 }>;
 
 export type CreateMonto = (
   name: string,
   address_urls: string[]
-) => Effect.Effect<MontoCreatedEvent, Error>;
+) => Effect.Effect<MontoRegisteredEvent, Error>;
 
-export const createMonto: CreateMonto = (name, address_urls) => {
-  const validName = name.length > 0 && name.length < 20;
-  const validAddressUrls =
-    address_urls.length < 10 && address_urls.every((v) => v.length > 0); // TODO: validate url format.
-
-  if (!validName) {
-    return Effect.fail(new Error("invalid name"));
-  }
-
-  if (!validAddressUrls) {
-    return Effect.fail(new Error("invalid address urls"));
-  }
-
-  return Effect.succeed({
-    type: "MontoCreatedEvent",
-    name,
-    address_urls,
-  });
+type MontoStatus = {
+  version: number;
+  id: string;
+  name: string;
+  addressUrls: string[];
+  removedReason: string | null;
 };
+
+export class Monto {
+  #events: ReadonlyArray<DeepReadonly<MontoEvent>>;
+  #status: DeepReadonly<MontoStatus>;
+
+  private constructor(
+    events: MontoEvent[] = [],
+    status: MontoStatus = {
+      version: 0,
+      id: "",
+      name: "",
+      addressUrls: [],
+      removedReason: null,
+    }
+  ) {
+    this.#events = events;
+    this.#status = status;
+  }
+
+  private append(event: MontoEvent) {
+    this.#events = [...this.#events, event];
+    this.apply(event);
+  }
+
+  private apply(event: MontoEvent) {
+    Match.type<MontoEvent>().pipe(
+      Match.tag("MontoRegisteredEvent", (event) => {
+        this.#status = {
+          version: 0,
+          id: crypto.randomUUID(),
+          name: event.name,
+          addressUrls: event.address_urls,
+          removedReason: null,
+        };
+      }),
+      Match.tag("MontoRemovedEvent", (event) => {
+        this.#status = {
+          ...this.#status,
+          removedReason: event.reason,
+        };
+      }),
+      Match.exhaustive
+    )(event);
+  }
+
+  public static replay(events: MontoEvent[]): Effect.Effect<Monto, Error> {
+    if (events.length < 1)
+      Effect.fail(new Error("events must be greater than 0"));
+
+    const monto = new Monto();
+    for (const event of events) {
+      monto.append(event);
+    }
+    return Effect.succeed(monto);
+  }
+}
